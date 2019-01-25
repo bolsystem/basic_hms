cf9de1df4a83345926666bba09196372bcea1992
Javier <soporte@bolsystem.com>
2019-01-25 18:08:37 -0400

    Error detectado en Crear Asegurado
    Error detectado en Crear Asegurado; cuando se crea un usuario nuevo.

--- model/medical_insurance.py
+++ model/medical_insurance.py
@@ -7,10 +7,10 @@ class medical_insurance(models.Model):
     _name = 'medical.insurance'
     _rec_name = 'insurance_compnay_id'
 
-    number = fields.Char('Numero')
+    number = fields.Char('Nro. de Poliza')
     medical_insurance_partner_id = fields.Many2one('res.partner','Propietario',required=True)
     patient_id = fields.Many2one('res.partner', 'Pertenece a')
-    type =  fields.Selection([('state','Estado'),('private','Privado'),('labour_union','Sindicato Laboral / Sindical')],'Tipo de seguro')
+    type =  fields.Selection([('state','Estado'),('private','Privado'),('labour_union','Sindicato Laboral / Sindical')],default="state", string='Proveedor del seguro')
     member_since= fields.Date('Miembro desde')
     insurance_compnay_id = fields.Many2one('res.partner',domain=[('is_insurance_company','=',True)],string='Compañia aseguradora')
     category = fields.Char('Categoria')
--- model/medical_patient.py
+++ model/medical_patient.py
@@ -51,7 +51,7 @@ class medical_patient(models.Model):
     date_of_death = fields.Datetime(string="Fecha de muerte")
     cause_of_death = fields.Char(string='Cause de la muerte')
     receivable = fields.Float(string="Cuenta x cobrar", readonly=True)
-    current_insurance_id = fields.Many2one('medical.insurance',string="Nro.Poliza")
+    current_insurance_id = fields.Many2one('medical.insurance',string="Comp.Aseguradora")
     procedencia = fields.Char( string="Lugar/Procedencia", help="Ej. Montero, Warnes. San Ignacio, Etc.")
     partner_address_id = fields.Many2one('res.partner', string="Direccion", )
     primary_care_physician_id = fields.Many2one('medical.physician', string="Medico Principal")
